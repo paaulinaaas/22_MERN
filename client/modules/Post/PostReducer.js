cd438ae7f342a338b192cb3b8ed8c118812440c1
paaulinaaas <psikora089@gmail.com>
2017-11-10 10:24:54 +0100

    Update PostReducer.js

--- client/modules/Post/PostReducer.js
+++ client/modules/Post/PostReducer.js
@@ -1,4 +1,4 @@
-import { ADD_POST, ADD_POSTS, DELETE_POST } from './PostActions';
+import { ADD_POST, ADD_POSTS, DELETE_POST, EDIT_POST } from './PostActions';
 
 // Initial State
 const initialState = { data: [] };
@@ -19,6 +19,34 @@ const PostReducer = (state = initialState, action) => {
       return {
         data: state.data.filter(post => post.cuid !== action.cuid),
       };
+
+      case EDIT_POST :
+  return {
+    data: state.data.map(post => { return post.cuid === action.cuid ? Object.assign({}, post, action.post) : post) },
+      };
+
+      case THUMB_UP_POST :
+      return {
+         data: state.data.map(function(post) {
+           if (post.cuid === action.cuid) {
+             return {...post, votes: post.votes + 1};          
+              } else {
+                return post;
+          }
+        })
+     }
+
+     case THUMB_DOWN_POST :
+     return {
+         data: state.data.map(function(post) {
+           if (post.cuid === action.cuid) {
+             return {...post, votes: post.votes - 1};
+           } else {
+             return post;
+           }
+         })
+       }
+
 
     default:
       return state;
